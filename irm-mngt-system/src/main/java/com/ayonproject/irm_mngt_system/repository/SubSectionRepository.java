17c03614c4aff15112eb03496b18587f09c449d4
Samuel Dominic Kamara <samueldominic246@gmail.com>
2025-03-05 20:17:01 +0000

    Deleted files and staged untracked files

--- irm-mngt-system/src/main/java/com/ayonproject/irm_mngt_system/repository/SubSectionRepository.java
+++
@@ -1,9 +0,0 @@
-package com.ayonproject.irm_mngt_system.repository;
-
-import com.ayonproject.irm_mngt_system.entity.SubSectionEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface SubSectionRepository extends JpaRepository<SubSectionEntity, Long> {
-}
